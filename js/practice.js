bc55479339af53054c072ed98655b49f9eaeca74
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-07-14 01:03:22 +0300

    Практика JS. Основы

---
+++ js/practice.js
@@ -0,0 +1,36 @@
+console.log(2 + 2);
+
+const userName = 'Heorhii';
+
+console.log(userName);
+
+let age = 28;
+
+console.log(age);
+
+let about = `I am ${userName} and I ${age} years old`;
+console.log(about);
+
+console.log(age);
+
+const hasChildren = false;
+const hasJob = true;
+
+console.log(typeof hasChildren);
+console.log(typeof age);
+
+// alert(about);
+
+const subAccess = 'Вы уверены, что хотите продлить подписку?';
+
+console.log(confirm(subAccess));
+
+let userAge = prompt('Пожалуйста, укажите свой возраст');
+
+console.log(userAge);
+
+
+
+
+
+

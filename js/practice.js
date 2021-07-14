076b81d4b702b5d3b5397ca5fcd00d35abd42e7b
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-07-14 17:33:12 +0300

    Практика JS. Основы + настройка автозаполнения

--- js/practice.js
+++ js/practice.js
@@ -21,13 +21,46 @@ console.log(typeof age);
 
 // alert(about);
 
-const subAccess = 'Вы уверены, что хотите продлить подписку?';
+// const subAccess = 'Вы уверены, что хотите продлить подписку?';
 
-console.log(confirm(subAccess));
+// console.log(confirm(subAccess));
 
-let userAge = prompt('Пожалуйста, укажите свой возраст');
+// let userAge = prompt('Пожалуйста, укажите свой возраст');
 
-console.log(userAge);
+// console.log(userAge);
+
+console.log(3 * 7);
+
+console.log(100 % 32);
+
+const x = 30;
+const y = 12;
+const z = 36;
+
+console.log(x > y);
+console.log((x * y) === z);
+console.log(x * y);
+console.log((x / 3) === true);
+
+const valueA = '5';
+console.log(Number(valueA));
+console.log(typeof Number(valueA));
+
+let numberAndString = Number('fadf');
+console.log(Number.parseInt(numberAndString));
+
+console.log(Number.isNaN(numberAndString));
+
+console.log(0.1 + 0.2);
+console.log((0.1 *10 + 0.2 * 10) / 10);
+
+console.log(Math.max( x, y, z));
+
+console.log(Math.random());
+console.log(Math.random() * (100 - 1) + 1);
+
+
+
 
 
 

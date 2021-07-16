9b8ab35b0ca6555679217b2c2626888269d09c7c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-07-16 15:54:54 +0300

    Преобразование к булю

---
+++ js/to-boolean.js
@@ -0,0 +1,32 @@
+let massage = 'Yo!';
+
+let toBoolean = Boolean(massage);
+
+console.log(massage);
+console.log(toBoolean);
+console.log(typeof toBoolean);
+
+massage = 'Yo!';
+
+toBoolean = !!massage;
+
+console.log(massage);
+console.log(toBoolean);
+console.log(typeof toBoolean);
+
+massage = '123';
+
+toBoolean = Boolean(massage);
+
+console.log(massage);
+console.log(toBoolean);
+console.log(typeof toBoolean);
+
+massage = 2;
+
+toBoolean = !!massage;
+
+console.log(massage);
+console.log(toBoolean);
+console.log(typeof toBoolean);
+

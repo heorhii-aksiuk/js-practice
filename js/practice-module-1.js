b932229327f4508cb1bc7c662753203e4f3a6606
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-07-16 17:51:16 +0300

    Практика по конспекту

---
+++ js/practice-module-1.js
@@ -0,0 +1,21 @@
+let result = 5 + 6 + '4';
+console.log(result);
+
+let massage = 'Hello my friend';
+console.log(massage.length);
+console.log(massage.toLowerCase());
+console.log(massage.toUpperCase());
+console.log(massage.indexOf('my'));
+console.log(massage.includes('My'));
+
+const randomNumber = 50;
+let result = randomNumber >= 10 && randomNumber < 40;
+console.log(result);
+
+result = randomNumber < 10 || randomNumber < 40;
+console.log(result);
+
+console.log(!true);
+
+
+

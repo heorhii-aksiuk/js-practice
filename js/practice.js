28fd2f400aad33187d04bc3ea6b717ef79c2d56c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-07-25 19:07:34 +0300

    Практика по дз

--- js/practice.js
+++ js/practice.js
@@ -205,15 +205,45 @@
 // getEvenNumbers(3, 11)
 
 
-function includes(array, value) {
+// function includes(array, value) {
   
-  for( let element of array) {
+//   for( let element of array) {
     
-    if (element === value){
-    return true;
-    }
-  }
-  return console.log(includes());
+//     if (element === value){
+//     return true;
+//     }
+//   }
+//   return console.log(includes());
+// }
+
+// includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
+
+
+const hotel = {
+  name: 'Resort Hotel',
+  stars: 5,
+  capacity: 100,
+};
+
+/*
+ * Посмотрите в консоли из каких двух элементов состоит каждый подмассив.
+ * Первый элемент это ключ свойства, второй это значение.
+ */
+const entries = Object.entries(hotel);
+
+/*
+ * На каждой итерации по entries выберем первый элемент подмассива (ключ)
+ * в переменную key, а второй (значение) в переменную value
+ */
+for (const entry of entries) {
+  const key = entry[0];
+  const value = entry[1];
+
+  console.log(`${key}: ${value}`);
 }
 
-includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
+/*
+ * name: Resort Hotel
+ * stars: 5
+ * capacity: 100
+ */

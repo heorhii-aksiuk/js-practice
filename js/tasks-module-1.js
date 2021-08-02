57e93139d4e482de852157ae5cfcd20d4a731fc2
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-02 21:17:34 +0300

    Пример из конспекта

--- js/tasks-module-1.js
+++ js/tasks-module-1.js
@@ -179,3 +179,21 @@
 //     },
 //   },
 // };
+
+const makeShef = function (name) {
+  /*
+   * Параметр name это локальная переменная для функции makeShef.
+   * Это значит что она будет доступна функции makeDish через замыкание.
+   */
+  return function makeDish(dish) {
+    console.log(`${name} is cooking ${dish}`);
+  };
+};
+
+const mango = makeShef('Mango');
+mango('apple pie'); // Mango is cooking apple pie
+mango('beef stew'); // Mango is cooking beef stew
+
+const poly = makeShef('Poly');
+poly('pancakes'); // Poly is cooking pancakes
+poly('eggs and bacon'); // Poly is cooking eggs and bacon

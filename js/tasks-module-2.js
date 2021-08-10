1dfeb30172505e3b73b883d889d402cb598d00d0
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-10 22:08:55 +0300

    Решение задачи 2, модуля 2

--- js/tasks-module-2.js
+++ js/tasks-module-2.js
@@ -14,31 +14,62 @@
 // Вернуть значение
 
 
-const capitalize = function (sentence) {
-  const sentenceArray = sentence.split(' ');
-  let letter;
-  let lettersArray = [];
-  let firstLetter;
-  let wordsArray = [];
-  let finalSentence;
+// const capitalize = function (sentence) {
+//   const sentenceArray = sentence.split(' ');
+//   let letter;
+//   let lettersArray = [];
+//   let firstLetter;
+//   let wordsArray = [];
+//   let finalSentence;
 
-  for (let word of sentenceArray) {
-    letter = word.split('');
-    lettersArray.push(letter);
-  }
+//   for (let word of sentenceArray) {
+//     letter = word.split('');
+//     lettersArray.push(letter);
+//   }
 
-  for (let i = 0; i < lettersArray.length; i += 1) {
-    firstLetter = lettersArray[i][0];
-    lettersArray[i].splice(0, 1, ' ', firstLetter.toUpperCase());
+//   for (let i = 0; i < lettersArray.length; i += 1) {
+//     firstLetter = lettersArray[i][0];
+//     lettersArray[i].splice(0, 1, ' ', firstLetter.toUpperCase());
   
-    for (let j = 0; j < lettersArray[i].length; j += 1) {
-      wordsArray += lettersArray[i][j];
-    }
-  } 
-  finalSentence = wordsArray.slice(1);
+//     for (let j = 0; j < lettersArray[i].length; j += 1) {
+//       wordsArray += lettersArray[i][j];
+//     }
+//   } 
+//   finalSentence = wordsArray.slice(1);
 
-  return console.log(finalSentence);
-}
+//   return console.log(finalSentence);
+// }
 
-capitalize('the quick brown fox');
-capitalize('hello my dear friend');
+// capitalize('the quick brown fox');
+// capitalize('hello my dear friend');
+
+// Задача 2
+
+// Написати ф-ю countVowels яка буде приймати строку і буде вертати кількість голосних літер. 
+// aeiouAEIOU - рядок з голосними в англ алфавіті
+
+// 1. Разбить строку на массив букв DONE!
+// 2. Перебрать буквы
+// 3. Если буква гласная, записать в результующую переменную +1
+
+const countVowels = function (sentence) {
+
+  const lettersArray = sentence.toLowerCase().split(''); 
+  let result = 0;
+
+  for (let letter of lettersArray) {
+  
+    if (letter === "a" ||
+        letter === "e" ||
+        letter === "i" ||
+        letter === "o" ||
+        letter === "u") {
+      result += 1;
+    };
+  };
+  return console.log(result);
+};
+
+countVowels('The Quick Brown Fox') // 5
+countVowels('hello my friend') // 4
+

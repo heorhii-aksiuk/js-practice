10158a8cba2bd80501575c01a976fd14896054c9
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-16 12:44:12 +0300

    Разбор дз

--- js/practice.js
+++ js/practice.js
@@ -469,28 +469,89 @@
 
 
 
-class Storage {
-  constructor(items) {
-    this.items = items;
-  };
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   };
   
-  getItems() {
-    return this.items;
-  };
+//   getItems() {
+//     return this.items;
+//   };
   
-  addItem(newItem) {
-    this.items.push(newItem);
-  };
+//   addItem(newItem) {
+//     this.items.push(newItem);
+//   };
   
-  removeItem(item) {
-    const itemIndex = this.items.indexOf(item);
-    this.items.splice(itemIndex, 1);
-  };
+//   removeItem(item) {
+//     const itemIndex = this.items.indexOf(item);
+//     this.items.splice(itemIndex, 1);
+//   };
+// };
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+
+function StringBuilder(baseValue) {
+  this.value = baseValue;
+}
+
+StringBuilder.prototype.getValue = function () {
+  return this.value;
 };
+
+StringBuilder.prototype.padEnd = function (str) {
+  this.value += str;
+};
+
+StringBuilder.prototype.padStart = function (str) {
+  this.value = str + this.value;
+};
+
+StringBuilder.prototype.padBoth = function (str) {
+  this.padStart(str);
+  this.padEnd(str);
+};
+
 // Пиши код выше этой строки
-const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
-storage.addItem("Дроид");
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
-storage.removeItem("Пролонгер");
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // '.'
+builder.padStart('^');
+console.log(builder.getValue()); // '^.'
+builder.padEnd('^');
+console.log(builder.getValue()); // '^.^'
+builder.padBoth('=');
+console.log(builder.getValue()); // '=^.^='
+
+
+
+
+class StringBuilder {
+
+  #value;
+
+  constructor(baseValue) {
+    this.#value = baseValue;
+  };
+  
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value += str;
+  }
+
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}

41bcc0fa987a9e26aeb3ad140fd53ee490c76b11
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-22 20:45:31 +0300

    Разбор дз

--- js/practice.js
+++ js/practice.js
@@ -711,14 +711,200 @@
 
   // 6.9
 
-  function changeEven(numbers, value) {
-    const changeNumbers = [];
+  // function changeEven(numbers, value) {
+  //   const changeNumbers = [];
 
-    numbers.forEach(number => number % 2 === 0 
-      ? changeNumbers.push(number + value) 
-      : changeNumbers.push(number));
+  //   numbers.forEach(number => number % 2 === 0 
+  //     ? changeNumbers.push(number + value) 
+  //     : changeNumbers.push(number));
 
-	  return changeNumbers;
+	//   return changeNumbers;
+  // }
+
+  // console.log(changeEven([2, 8, 3, 7, 4, 6], 10))
+
+// 6.14
+
+
+  // Пиши код ниже этой строки
+// const getUserEmails = users => {
+//   return users.map(user => user.email)
+
+//   };
+  // Пиши код выше этой строки
+
+  // 6.16
+
+  // const books = [
+  //   {
+  //     title: 'Последнее королевство',
+  //     author: 'Бернард Корнуэлл',
+  //     genres: ['приключения', 'историческое']
+  //   },
+  //   {
+  //     title: 'На берегу спокойных вод',
+  //     author: 'Роберт Шекли',
+  //     genres: ['фантастика', 'мистика']
+  //   },
+  //   {
+  //     title: 'Красна как кровь',
+  //     author: 'Ли Танит',
+  //     genres: ['ужасы', 'мистика', 'приключения']
+  //   }
+  // ];
+  // // Пиши код ниже этой строки
+  // const allGenres = books.map(book => book.genres);
+  // const uniqueGenres = allGenres.filter((ganre, index, array) => array.indexOf(ganre) === index);
+
+
+  // 6.17
+
+/*   Используя метод filter() дополни код так, чтобы:
+В переменной topRatedBooks получился массив книг рейтинг которых (свойство rating)
+ больше либо равно значению переменной MIN_RATING.
+В переменной booksByAuthor получился массив книг написанных автором с именем (свойство author) 
+которое совпадает со значением в переменной AUTHOR.
+ */
+//   const books = [
+//   { title: 'Последнее королевство', author: 'Бернард Корнуэлл', rating: 8.38 },
+//   { title: 'На берегу спокойных вод', author: 'Роберт Шекли', rating: 8.51 },
+//   { title: 'Сон смешного человека', author: 'Федор Достоевский', rating: 7.75 },
+//   { title: 'Красна как кровь', author: 'Ли Танит', rating: 7.94 },
+//   { title: 'Враг Божий', author: 'Бернард Корнуэлл', rating: 8.67 }
+// ];
+
+// const MIN_RATING = 8;
+// const AUTHOR = 'Бернард Корнуэлл';
+// // Пиши код ниже этой строки
+
+// const topRatedBooks = books.filter(({rating}) => rating >= MIN_RATING);
+// const booksByAuthor = books.filter(({author}) => author === AUTHOR);
+
+// 6.18
+
+// const getUsersWithEyeColor = (users, color) => {
+//  users.filter(({eyeColor, name }) => (eyeColor === color) {
+//  	return name })
+
+// };
+
+// const getUsersWithEyeColor = (users, color) => {
+//  users.filter(user => if (user.eyeColor === color)
+//      user.name };
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
   }
+]
 
-  console.log(changeEven([2, 8, 3, 7, 4, 6], 10))
+/* Дополни функцию getUsersWithEyeColor(users, color) так, 
+чтобы она возвращала массив пользователей у которых цвет глаз (свойство eyeColor) 
+совпадает со значением параметра color. */
+
+
+// const getUserEmails = users => {
+//   return users.map(user => user.email)
+
+//   };
+
+// console.log(getUserEmails(users))
+
+
+// const getUsersWithEyeColor = (users, color) => {
+//   return users.filter(user => {
+//     if(user.eyeColor === color){
+//       return [user.name]
+//     }})
+
+// };
+
+// console.log(getUsersWithEyeColor(users, 'brown'))
+// console.log()
+
+// const getUsersWithAge = (users, minAge, maxAge) => {
+//  const result = users.filter(user => user.age > minAge && user.age <=maxAge)
+// return result
+// };
+
+// console.log(getUsersWithAge(users, 20, 30))
+
+
+// const getFriends = (users) => {
+//   return users.flatMap(user => user.friends).filter((user, index, array) => array.indexOf(user) === index)
+// };
+
+// console.log(getFriends(users))
+
+const getActiveUsers = (users) => {
+   return users.filter(user => user.isActive)
+};
+
+console.log(getActiveUsers(users))

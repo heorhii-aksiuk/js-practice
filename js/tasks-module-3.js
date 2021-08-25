bfcbc67f9cfbbedfd88fc36667246f643d91416a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-25 21:18:18 +0300

    Решение задачи 2, модуля 3

--- js/tasks-module-3.js
+++ js/tasks-module-3.js
@@ -11,20 +11,38 @@ const orderB = { orange: 10, pork: 2, bread: 1 } */
 2. Вернуть цену заказа
 */
 
-const totalPrice = function (order, prices) {
-  let total = 0;
+// const totalPrice = function (order, prices) {
+//   let total = 0;
 
-  for (let product in order) {
-    for (let price in prices) {
-      if (product === price) {
-        total += order[product] * prices[price];
-      }
-    }
-  }
-  return total;
-}
+//   for (let product in order) {
+//     for (let price in prices) {
+//       if (product === price) {
+//         total += order[product] * prices[price];
+//       }
+//     }
+//   }
+//   return total;
+// }
 
-const productsPrice = { apple: 20, orange: 5, cheese: 12, bread: 23 };
-const orderA = { apple: 5, cheese: 1, bread: 3 };
+// const productsPrice = { apple: 20, orange: 5, cheese: 12, bread: 23 };
+// const orderA = { apple: 5, cheese: 1, bread: 3 };
 
-totalPrice(orderA, productsPrice); // 181
+// totalPrice(orderA, productsPrice); // 181
+
+// Task 2
+
+/* Написати ф-ю isObjectEmpty яка перевіряє чи обєкт пустий чи ні. 
+Якщо обєкт пустий то повернути true інакше false
+isObjectEmpty({}) // true
+isObjectEmpty({name: 'user, age: {21}}) // false */
+
+
+// const isObjectEmpty = function (object) {
+//   return Object.keys(object).length === 0
+// };
+
+const isObjectEmpty = object => Object.keys(object).length === 0;
+
+
+console.log(isObjectEmpty({})) // true
+console.log(isObjectEmpty({name: 'user', age: 21})) // false

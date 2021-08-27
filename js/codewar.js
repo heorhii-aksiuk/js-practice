874b6b99827c2f24d7bfc167a0360e10f2c494c8
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-27 21:45:48 +0300

    Начало решения задачи 1 по codewars

--- js/codewar.js
+++ js/codewar.js
@@ -6,6 +6,18 @@ If anything in the text isn't a letter, ignore it and don't return it.
 
 "a" = 1, "b" = 2, etc. */
 
-function alphabetPosition(text) {
-  return text;
-}
+/* Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string) */
+
+/* 
+1. Превратить строку в массив
+2. Перебрать массив
+3. 
+
+
+
+*/
+
+
+

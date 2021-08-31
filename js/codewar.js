6ad8137d37ac9f69d9cfcc24b457052037ce88b9
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-08-31 12:13:07 +0300

    Разбор задачи codewars

--- js/codewar.js
+++ js/codewar.js
@@ -21,15 +21,20 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 
 
-function validatePIN (pin) {
-  if ((pin.length === 4 || pin.length === 6) && !Number.isNaN(pin)) {
-    console.log(pin.length);
-    return true
-  } else {
-    return false
-  }
-}
-const pin = "1234"
-console.log(!Number.isNaN(pin));
+const validatePIN = function (pin) {
+  let filteredArray = pin.split('').filter(el => el >= 0 && el <= 9);
+  
+  return filteredArray.length === pin.length
+    && (pin.length === 4 || pin.length === 6)
+  ? true
+  : false;
+};
+
+console.log(validatePIN("0089")); // true
+console.log(validatePIN("-1.234")); // false
+console.log(validatePIN("098765")); // true
+console.log(validatePIN("123")); // false
+
+
 
 

78afa187eaef83b2a57acfeaef3bea88d5344217
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-01 01:11:09 +0300

    Решение задачи codewars

--- js/codewar.js
+++ js/codewar.js
@@ -15,17 +15,15 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 2. Перебрать массив
 3. 
 
-
-
 */
 
-
-
 const validatePIN = function (pin) {
-  let filteredArray = pin.split('').filter(el => el >= 0 && el <= 9);
+  let clearPin = pin.trim();
+  let filteredArray = clearPin.split('')
+    .filter(el => el >= 0 && el <= 9);
   
-  return filteredArray.length === pin.length
-    && (pin.length === 4 || pin.length === 6)
+  return filteredArray.length === clearPin.length
+    && (clearPin.length === 4 || clearPin.length === 6)
   ? true
   : false;
 };
@@ -34,7 +32,5 @@ console.log(validatePIN("0089")); // true
 console.log(validatePIN("-1.234")); // false
 console.log(validatePIN("098765")); // true
 console.log(validatePIN("123")); // false
-
-
 
 

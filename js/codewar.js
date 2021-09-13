68e01947159581fed9c37bc78c0049e26c039dd6
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-09-14 00:34:20 +0300

    Codewar tasks practice

--- js/codewar.js
+++ js/codewar.js
@@ -21,7 +21,7 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 //   let clearPin = pin.trim();
 //   let filteredArray = clearPin.split('')
 //     .filter(el => el >= 0 && el <= 9);
-  
+
 //   return filteredArray.length === clearPin.length
 //     && (clearPin.length === 4 || clearPin.length === 6)
 //   ? true
@@ -33,14 +33,33 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 // console.log(validatePIN("098765")); // true
 // console.log(validatePIN("123")); // false
 
+// const removeChar = function(str){
+//   let arrayFromStr = str.split('');
+//   arrayFromStr.shift();
+//   arrayFromStr.pop();
 
-const removeChar = function(str){
-  let arrayFromStr = str.split('');
-  arrayFromStr.shift();
-  arrayFromStr.pop();
+//   return arrayFromStr.join('');
+// };
 
-  return arrayFromStr.join('');
-};
+// removeChar('eloquent');
+// console.log(removeChar('eloquent'));
 
-removeChar('eloquent');
-console.log(removeChar('eloquent'));
+// const findShort = function (s) {
+//   let array = s.split(' ');
+//   let sortArray = [...array].sort((a, b) => a.length - b.length);
+//   return sortArray[0].length;
+// };
+
+// const findShort = function (s) {
+//   return s.split(' ').sort((a, b) => a.length - b.length)[0].length;
+// };
+
+// findShort('bitcoin take over the world maybe who knows perhaps');
+
+function minMax(arr) {
+  return [[...arr].sort((a, b) => a - b)[0], [...arr].sort((a, b) => b - a)[0]];
+}
+
+const arr = [5, 3, 7, 9, 34, 56, 3, 5, 3, 7];
+minMax(arr);
+console.log(minMax(arr));

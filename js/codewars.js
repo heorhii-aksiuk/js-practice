5b012a135717f48f16a2cd8eebca6850045a8904
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-01-13 23:38:35 +0200

    codewars task uncorrect solution

--- js/codewars.js
+++ js/codewars.js
@@ -184,7 +184,22 @@ Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 1
 
 // console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]));
 
-function findOdd(A) {
-  //happy coding!
-  return 0;
+function uniqueInOrder(iterable) {
+  let inboundArray = [];
+  let resultArray = [];
+
+  if (typeof iterable === 'string') {
+    inboundArray = iterable.split('');
+  } else {
+    inboundArray = iterable;
+  }
+
+  inboundArray.forEach(el => {
+    if (!resultArray.includes(el)) resultArray.push(el);
+  });
+
+  return resultArray;
 }
+
+console.log(uniqueInOrder('ABBCcAD'));
+console.log(uniqueInOrder([1, 2, 2, 3, 3]));

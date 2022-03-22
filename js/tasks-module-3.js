44bd4e0d891040d6a20cb64741e9aefd360e0012
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-03-22 22:45:16 +0200

    pizza object refactoring

--- js/tasks-module-3.js
+++ js/tasks-module-3.js
@@ -125,31 +125,27 @@ isObjectEmpty({name: 'user, age: {21}}) // false */
 // console.table(pizza)
 
 // Task 3
-
 /* Створити обєкт піци з полями і методами
-
 В обєкті має бути такі поля і методи:
-
 1. DONE! Поле для збереження розміру піци
 2. DONE! Поле для збереження списку добавок
 3. DONE! Поле для збереження списку соусів
-4. DONE! Метод для додавання добавки (Можна додати добавку якщо вона відсутня інакше показувати помилку)
-5. DONE! Метод для додавання соусу (Можна додати соус якщо він відсутній інакше показувати помилку)
-6. DONE! Метод для видалення добавки (Можна видалити добавку якщо вона присутня в піці інакше показувати помилку, також помилка показувється якщо пробуємо видалити соус, а там ще жодного немає)
-7. DONE! Метод для видалення соусу (Можна видалити соус якщо він присутній в піці інакше показувати помилку)
+4. DONE! Метод для додавання добавки (Можна додати добавку якщо вона відсутня, інакше показувати помилку)
+5. DONE! Метод для додавання соусу (Можна додати соус якщо він відсутній, інакше показувати помилку)
+6. DONE! Метод для видалення добавки (Можна видалити добавку якщо вона присутня в піці, інакше показувати помилку)
+7. DONE! Метод для видалення соусу (Можна видалити соус якщо він присутній в піці, інакше показувати помилку, також помилка показувється якщо пробуємо видалити соус, а там ще жодного немає)
 8. DONE! Метод для розрахунку загальної ціни піци (розмір + добавки + соуси) (ціна округлена до двох знаків після коми)
 9. DONE! Метод для розрахунку загальної кількості калорій піци (розмір + добавки + соуси)
 10. DONE! Метод який показує загальну інформацію про замовлення (ціну, калорії, список добавок і соусів) (Якщо піца валідна інакше показувати помилку)
 11. DONE! Метод вибору розміру піци (Розмір піци можна змінити в будь-який момент)
 12. DONE! Метод що показує час приготуванни в хвилинах в залежності від складності піци
-13. Метод валідації піци який поверне true якщо піца відповідає вимогам , а саме (вибраний розмір піци, є мінімум одна добавка, і мінімум один соус) інакше false (І виводить строку чого не вистачає (коржа, добавки, чи соусу)) (використовувати this)
-Створити всі методи і перевірити чи вони працюють 
+13. DONE! Метод валідації піци який поверне true якщо піца відповідає вимогам , а саме (вибраний розмір піци, є мінімум одна добавка, і мінімум один соус) інакше false (І виводить строку чого не вистачає (коржа, добавки, чи соусу))
 */
 
 const SIZES = {
   BIG: { name: 'big', price: 25, cal: 100, time: 15 },
+  MEDIUM: { name: 'medium', price: 15, cal: 75, time: 10 },
   SMALL: { name: 'small', price: 15, cal: 50, time: 7 },
-  MEDIUM: { name: 'medium', price: 15, cal: 50, time: 7 },
 }
 const STUFFING = {
   CHEESE: { name: 'cheese', price: 6.5, cal: 45, time: 2 },
@@ -170,7 +166,7 @@ const pizza = {
 
   changeSize(newSize) {
     this.size = newSize
-    return `Size changing to ${newSize.name}`
+    return `Pizza size changing to ${newSize.name}.`
   },
 
   addStuffing(stuffing) {
@@ -178,7 +174,7 @@ const pizza = {
       return `Stuffing ${stuffing.name} is alredy added!`
     }
     this.stuffing.push(stuffing)
-    return `Stuffing ${stuffing.name} added!`
+    return `Stuffing ${stuffing.name} added.`
   },
 
   addSouce(souce) {
@@ -186,31 +182,33 @@ const pizza = {
       return `Souce ${souce.name} is alredy added!`
     }
     this.souces.push(souce)
-    return `Souce ${souce.name} added!`
+    return `Souce ${souce.name} added.`
   },
 
   deleteStuffing(stuffing) {
-    if (this.stuffing.length === 0) return `Error! Stuffing is empty!`
-
+    if (this.stuffing.length === 0) {
+      return `Error! Stuffing is empty!`
+    }
     const index = this.stuffing.findIndex(({ name }) => name === stuffing.name)
 
     if (index === -1) {
       return `Error! Stuffing ${stuffing.name} is missing!`
     }
     this.stuffing.splice(index, 1)
-    return `Stuffing ${stuffing.name} is deleted!`
+    return `Stuffing ${stuffing.name} is deleted.`
   },
 
   deleteSouce(souce) {
-    if (this.souces.length === 0) return `Error! Souces is empty!`
-
+    if (this.souces.length === 0) {
+      return `Error! Souces is empty!`
+    }
     const index = this.souces.findIndex(({ name }) => name === souce.name)
 
     if (index === -1) {
       return `Error! Souce ${souce.name} is missing!`
     }
     this.souces.splice(index, 1)
-    return `Souce ${souce.name} is deleted!`
+    return `Souce ${souce.name} is deleted.`
   },
 
   getPrice() {
@@ -261,41 +259,78 @@ const pizza = {
     return `Total cooking time: ${totalTime} minutes.`
   },
 
-  getInfo() {
-    if (this.stuffing.length === 0 && this.souces.length === 0) {
-      return `Error! Stuffing and souces are empty!`
-    }
-
-    if (this.stuffing.length === 0) return `Error! Stuffing is empty!`
-    if (this.souces.length === 0) return `Error! Souces is empty!`
-
-    const sizeInfo = this.size.name
-    const stuffingInfo = this.stuffing.map(({ name }) => name).join(',')
-    const soucesInfo = this.souces.map(({ name }) => name).join(',')
-
-    return `Pizza size: ${sizeInfo}. Stuffing: ${stuffingInfo}. Souces: ${soucesInfo}. ${this.geCalories()} ${this.getPrice()}`
+  isValid() {
+    const valid =
+      this.size && this.stuffing.length > 0 && this.souces.length > 0
+    if (!valid) console.log(this.getInfo())
+    return valid
   },
 
-  isValid() {
-    return this.size && this.stuffing.length > 0 && this.souces.length > 0
+  getInfo() {
+    let infoMessage = 'Error!'
+
+    if (!this.size) infoMessage += ' Size is empty!'
+    if (this.stuffing.length === 0) infoMessage += ' Stuffing is empty!'
+    if (this.souces.length === 0) infoMessage += ' Souces is empty!'
+
+    const valid =
+      this.size && this.stuffing.length > 0 && this.souces.length > 0
+
+    if (valid) {
+      const sizeInfo = `Pizza size: ${this.size.name}.`
+      const stuffingInfo = `Stuffing: ${this.stuffing
+        .map(({ name }) => name)
+        .join(', ')}.`
+      const soucesInfo = `Souces: ${this.souces
+        .map(({ name }) => name)
+        .join(', ')}.`
+
+      infoMessage = `
+      ${sizeInfo}
+      ${stuffingInfo} 
+      ${soucesInfo} 
+      ${this.getCookingTime()} 
+      ${this.geCalories()} 
+      ${this.getPrice()}
+      `
+    }
+    return infoMessage
   },
 }
 
 console.table(pizza)
+console.log(pizza.size)
+console.log(pizza.stuffing)
+console.log(pizza.souces)
 
-// console.log(pizza.size.name)
+console.log(pizza.changeSize(SIZES.BIG))
+
+console.log(pizza.deleteStuffing(STUFFING.CHEESE))
+console.log(pizza.deleteSouce(SAUCES.MUSTARD))
+
 console.log(pizza.addStuffing(STUFFING.BEACON))
-// console.log(pizza.deleteStuffing(STUFFING.CHEESE))
-// console.log(pizza.deleteStuffing(STUFFING.BEACON))
+console.log(pizza.addStuffing(STUFFING.BEACON))
+console.log(pizza.addStuffing(STUFFING.CHEESE))
+console.log(pizza.addStuffing(STUFFING.TOMATO))
+console.log(pizza.addStuffing(STUFFING.CHICKEN))
 
-// console.log(pizza.addStuffing(STUFFING.BEACON))
+console.log(pizza.deleteStuffing(STUFFING.CHEESE))
+console.log(pizza.deleteStuffing(STUFFING.CHEESE))
+
 console.log(pizza.addSouce(SAUCES.BOLOGNESE))
-// console.log(pizza.deleteSouce(SAUCES.MUSTARD))
+console.log(pizza.addSouce(SAUCES.BOLOGNESE))
+console.log(pizza.addSouce(SAUCES.KETCHUP))
+console.log(pizza.addSouce(SAUCES.MUSTARD))
+
+console.log(pizza.deleteSouce(SAUCES.MUSTARD))
+console.log(pizza.deleteSouce(SAUCES.MUSTARD))
+
+console.log(pizza.size)
+console.log(pizza.stuffing)
+console.log(pizza.souces)
+
 console.log(pizza.getPrice())
 console.log(pizza.geCalories())
 console.log(pizza.getCookingTime())
+console.log(pizza.getInfo())
 console.log(pizza.isValid())
-
-// console.log(pizza.addSouce(SAUCES.BOLOGNESE))
-
-console.table(pizza)

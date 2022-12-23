e601b54cd2e0e1f5dc9f10e61d544511e9de4f7f
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-12-23 17:16:40 +0200

    practice

--- js/practice2.js
+++ js/practice2.js
@@ -87,24 +87,72 @@
 // audi.price = 51000;
 // console.log(audi.price); // 49000
 
-function findPrimeNumbers(maximum) {
-  const resultArray = [];
+// function findPrimeNumbers(maximum) {
+//   const resultArray = [];
 
-  for (let i = 1; i <= maximum; i += 2) {
-    resultArray.push(i);
+//   for (let i = 1; i <= maximum; i += 2) {
+//     resultArray.push(i);
 
-    for (let j = i; j > 2; j -= 1) {
-      if (i === j) {
-        continue;
-      }
-      if (i % j === 0) {
-        resultArray.pop();
-        break;
-      }
-    }
-  }
+//     for (let j = i; j > 2; j -= 1) {
+//       if (i === j) {
+//         continue;
+//       }
+//       if (i % j === 0) {
+//         resultArray.pop();
+//         break;
+//       }
+//     }
+//   }
 
-  return resultArray;
+//   return resultArray;
+// }
+
+// console.log(findPrimeNumbers(100));
+
+// function countBy(x, n) {
+//   let z = [];
+//   for (let i = 1; i <= n; i += 1) {
+//     z.push(i * x);
+//   }
+//   return z;
+// }
+
+// console.log(countBy(2, 5));
+
+// function logicalCalc(array, op) {
+//   if (op === 'AND') {
+//     return array.every((item) => item);
+//   }
+
+//   if (op === 'OR') {
+//     return array.some((item) => item);
+//   }
+
+//   if (op === 'XOR') {
+//     return array.every((item) => item) ? false : array.some((item) => item);
+//   }
+// }
+
+// console.log(logicalCalc([true, true, true, false], 'XOR'));
+
+// const isSquare = (number) => Math.sqrt(number) % 1 === 0;
+
+// console.log(isSquare(-26));
+
+/* At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years. */
+
+function nbYear(p0, percent, aug, p) {
+  const oneYearGrow = p0 + p0 * (percent / 100) + aug - p0;
+  return (p - p0) / oneYearGrow;
 }
 
-console.log(findPrimeNumbers(100));
+console.log(nbYear(1500, 5, 100, 5000)); //15
